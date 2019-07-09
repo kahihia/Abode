4b8e6d87bec117bec1b4d86638db63090afc3ace
Samuel nganga <samien09@gmail.com>
2019-07-09 12:40:53 +0300

    Update keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 // export module file to be available to other components
 // mongoURI to connect with mongo database
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.MONGODB_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
   email:process.env.EMAIL,
   password:process.env.PASSWORD
